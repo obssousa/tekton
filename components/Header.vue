3fc6dd729686e22f5c9d6d8fa725dcc4455173a9
Bruno Sousa <bssbrubs@gmail.com>
2023-04-01 13:18:29 -0300

    refactor: add mobile navigator

--- components/Header.vue
+++ components/Header.vue
@@ -1,27 +1,37 @@
 <template>
-  <header class="navbar bg-white rounded-3xl my-4 mx-6 w-auto px-6">
-    <div class="flex-none lg:hidden">
-      <button class="btn btn-square btn-ghost">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-          class="inline-block w-5 h-5 stroke-current">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
-        </svg>
-      </button>
-    </div>
+  <header class="navbar bg-white rounded-3xl px-4 mx-2 my-3 lg:my-4 lg:mx-6 w-auto lg:px-6">
     <div class="flex-1 gap-3">
       <img :src="Logo" class="h-10" />
-      <a class="font-semibold text-xl text-primary-40">Bruna Sousa Arquitetura</a>
+      <a class="font-semibold text-base lg:text-xl text-primary-40">Bruna Sousa Arquitetura</a>
     </div>
-    <div class="flex-none">
+    <div class="hidden flex-none lg:block">
       <ul class="menu menu-horizontal px-1 gap-3">
         <li v-for="link in links" :key="link.title">
           <NuxtLink :to="link.route" :class="isLinkActive(link.route)
             ? 'block rounded md:bg-transparent text-primary-50 hover:text-primary-40'
-            : 'block text-neutral-700 hover:bg-slate-200 rounded hover:text-primary-50'">
+            : 'block text-neutral-700 hover:bg-slate-100 rounded hover:text-primary-50'">
             {{ link.title }}
           </NuxtLink>
         </li>
       </ul>
+    </div>
+    <div class="navbar-end w-auto lg:hidden">
+      <div class="dropdown">
+        <label tabindex="0" class="btn btn-ghost btn-circle">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" />
+          </svg>
+        </label>
+        <ul tabindex="0" class="menu menu-compact dropdown-content -right-4 mt-3 p-2 shadow bg-white rounded-box w-52">
+          <li v-for="link in links" :key="link.title">
+            <NuxtLink :to="link.route" :class="isLinkActive(link.route)
+              ? 'block rounded md:bg-transparent text-primary-50 hover:text-primary-40'
+              : 'block text-neutral-700 hover:bg-slate-100 rounded hover:text-primary-50'">
+              {{ link.title }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </div>
     </div>
   </header>
 </template>
