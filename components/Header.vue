ce7c034899ab12c125665f8a133e1e842f293cc1
Bruno Sousa <bssbrubs@gmail.com>
2023-04-01 13:23:23 -0300

    fix: header icon

--- components/Header.vue
+++ components/Header.vue
@@ -18,7 +18,8 @@
     <div class="navbar-end w-auto lg:hidden">
       <div class="dropdown">
         <label tabindex="0" class="btn btn-ghost btn-circle">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-primary-40 -scale-y-100  -rotate-180" fill="none"
+            viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" />
           </svg>
         </label>
